98c23a864b273cb6f24943f30d65744e2078cde1
CaioQuerino <caioquerino04@gmail.com>
2024-05-23 16:55:51 -0300

    Arquivos enviados

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+function lerDados() {
+    var nome = document.getElementById("idNome").value;
+    var username = document.getElementById("idUsuario").value;
+    var email = document.getElementById("idEmail").value;
+    var tel = document.getElementById("idCelular").value;
+    var password = document.getElementById("idSenha").value;
+    var confirmpassword = document.getElementById("idConfirmeSenha").value;
+
+    document.getElementById("nome").innerHTML = nome;
+    document.getElementById("username").innerHTML = username;
+    document.getElementById("email").innerHTML = email;
+    document.getElementById("tel").innerHTML = tel;
+    document.getElementById("password").innerHTML = password;
+    document.getElementById("confirmpassword").innerHTML = confirmpassword;
+
+    return false;
+}
+
+function esquecer(){
+    
+}

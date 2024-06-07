383114cb75ccbec3db4bbe3ae36ede0470f87af1
CaioQuerino <caioquerino04@gmail.com>
2024-06-06 21:17:54 -0300

    Arquivos enviados

--- src/js/script.js
+++ src/js/script.js
@@ -24,8 +24,8 @@ function lerDados() {
 }
 
 function fazerLogin() {
-  let email = document.getElementById("emailTelefone").value;
-  let senha = document.getElementById("senhaLogin").value;
+ email = document.getElementById("emailTelefone").value;
+ senha = document.getElementById("senhaLogin").value;
 
   // Verificar se as credenciais correspondem às armazenadas localmente
   let emailArmazenado = localStorage.getItem("email");
@@ -61,5 +61,21 @@ function esqueceu() {
   return false;
 }
 
-let
 
+document.addEventListener("DOMContentLoaded", () => {
+  const phoneInput = document.getElementById("idCelular");
+  phoneInput.addEventListener("input", handlePhone);
+});
+
+const handlePhone = (event) => {
+  let input = event.target;
+  input.value = phoneMask(input.value);
+};
+
+const phoneMask = (value) => {
+  if (!value) return "";
+  value = value.replace(/\D/g, '');
+  value = value.replace(/(\d{2})(\d)/, "($1) $2");
+  value = value.replace(/(\d)(\d{4})$/, "$1-$2");
+  return value;
+};
